ffe902b2a3e6bf27e63f1dabfb13444554f24ca0
Hunter <aloishis89@gmail.com>
2023-04-23 08:05:04 -0700

    Fixed output type

--- src/matrix.v
+++ src/matrix.v
@@ -4,11 +4,12 @@
 
 module blueintegral_mat_mult(
   input [7:0] input_data,
-  output reg [7:0] output_data
+  output [7:0] output_data
 );
 
 reg [1:0] A [1:0][1:0], B [1:0][1:0];
 reg [1:0] temp [1:0][1:0];
+reg [7:0] output_data_reg;
 
 always @* begin
 
@@ -34,19 +35,22 @@ temp[1][1] = (A[1][0]*B[0][1]) + (A[1][1]*B[1][1]);
 //combine into output
 
 //temp[0][0] = 2;
-output_data = 8'b00000000;
-output_data = output_data | {temp[0][0], 6'b000000};
-output_data = output_data | {2'b00, temp[0][1], 4'b0000};
-output_data = output_data | {4'b0000, temp[1][0], 2'b00};
-output_data = output_data | {6'b000000, temp[1][1]};
+output_data_reg = 8'b00000000;
+output_data_reg = output_data_reg | {temp[0][0], 6'b000000};
+output_data_reg = output_data_reg | {2'b00, temp[0][1], 4'b0000};
+output_data_reg = output_data_reg | {4'b0000, temp[1][0], 2'b00};
+output_data_reg = output_data_reg | {6'b000000, temp[1][1]};
+
 
 //temp[1][0] = 2;
 //output_data = {temp[0][1]};
 
 
-
-
 end
+
+assign output_data = output_data_reg;
+
+
 
 
 endmodule
